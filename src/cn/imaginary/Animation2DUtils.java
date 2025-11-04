75d784496f78047d1a206a6686f978b0d86092d5
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-11-04 21:55:22 +0800

    Update and rename src/cn/imaginary/Animation2DUtils.java to src/cn/imaginary/toolkit/Animation.java

--- src/cn/imaginary/Animation2DUtils.java
+++
@@ -1,6 +0,0 @@
-package cn.imaginary;
-
-public class Animation2DUtils {
-    public Animation2DUtils() {
-    }
-}
---
+++ src/cn/imaginary/toolkit/Animation.java
@@ -0,0 +1,6 @@
+package cn.imaginary;
+
+public class Animation {
+    public Animation() {
+    }
+}
