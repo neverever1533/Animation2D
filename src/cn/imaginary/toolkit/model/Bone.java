96403e5b65f173f3b45f5696e268cfc4d3f0e35b
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2025-11-12 18:38:51 +0800

    更新 Bone.java

--- src/cn/imaginary/toolkit/model/Bone.java
+++ src/cn/imaginary/toolkit/model/Bone.java
@@ -8,21 +8,22 @@ import java.util.Properties;
 public class Bone {
     private String name;
 
-    public String type = "bone";
+    public static String type = "bone";
 
     private boolean isVisible;
 
     private Point point_Location;
     private Point point_Scale;
     private Point point_Scale_Global;
+    private Point point_Scale_Local;
     private Point point_Translation;
     private Point point_Translation_Global;
+    private Point point_Translation_Local;
 
-    private double rotation;
     private double rotation_Degrees;
-    private double rotation_Global;
     private double rotation_Global_Degrees;
     private double rotation_Gravity_Degrees;
+    private double rotation_Local_Degrees;
 
     private int length_draw;
 
@@ -44,22 +45,6 @@ public class Bone {
         this.point_Location = point;
     }
 
-    public double getGlobalRotation() {
-        return rotation_Global;
-    }
-
-    public void setGlobalRotation(double theta) {
-        rotation_Global += theta;
-    }
-
-    public double getRotation() {
-        return rotation;
-    }
-
-    public void setRotation(double theta) {
-        rotation = theta;
-    }
-
     public double getGravityRotationDegrees() {
         return rotation_Gravity_Degrees;
     }
@@ -74,6 +59,14 @@ public class Bone {
 
     public void setGlobalRotationDegrees(double angle) {
         rotation_Global_Degrees += angle;
+    }
+
+    public double getLocalRotationDegrees() {
+        return rotation_Local_Degrees;
+    }
+
+    public void setLocalRotationDegrees(double angle) {
+        rotation_Local_Degrees = angle;
     }
 
     public double getRotationDegrees() {
@@ -99,8 +92,22 @@ public class Bone {
         setGlobalScale(point);
     }
 
-    private void setGlobalScale(Point point) {
+    public void setGlobalScale(Point point) {
         point_Scale_Global = point;
+    }
+
+    public Point getLocalScale() {
+        return point_Scale_Local;
+    }
+
+    public void setLocalScale(double scaleX, double scaleY) {
+        Point point = new Point();
+        point.setLocation(scaleX, scaleY);
+        setLocalScale(point);
+    }
+
+    public void setLocalScale(Point point) {
+        point_Scale_Local = point;
     }
 
     public Point getScale() {
@@ -116,7 +123,7 @@ public class Bone {
         setScale(point);
     }
 
-    private void setScale(Point point) {
+    public void setScale(Point point) {
         point_Scale = point;
     }
 
@@ -135,8 +142,22 @@ public class Bone {
         setGlobalTranslation(point);
     }
 
-    private void setGlobalTranslation(Point point) {
+    public void setGlobalTranslation(Point point) {
         point_Translation_Global = point;
+    }
+
+    public Point getLocalTranslation() {
+        return point_Translation_Local;
+    }
+
+    public void setLocalTranslation(double x, double y) {
+        Point point = new Point();
+        point.setLocation(x, y);
+        setLocalTranslation(point);
+    }
+
+    public void setLocalTranslation(Point point) {
+        point_Translation_Local = point;
     }
 
     public Point getTranslation() {
@@ -152,7 +173,7 @@ public class Bone {
         setTranslation(point);
     }
 
-    private void setTranslation(Point point) {
+    public void setTranslation(Point point) {
         point_Translation = point;
     }
 
@@ -196,21 +217,158 @@ public class Bone {
         Properties properties = new Properties();
         properties.put("type", getType());
         properties.put("name", getName());
-        properties.put("x", getLocation().getX());
-        properties.put("y", getLocation().getY());
+        Point location = getLocation();
+        if (null != location) {
+            properties.put("x", location.getX());
+            properties.put("y", location.getY());
+        }
         properties.put("globalTranslationX", getGlobalTranslation().getX());
         properties.put("globalTranslationY", getGlobalTranslation().getY());
+        Point localTranslation = getLocalTranslation();
+        if (null != localTranslation) {
+            properties.put("localTranslationX", localTranslation.getX());
+            properties.put("locallTranslationY", localTranslation.getY());
+        }
         properties.put("translationX", getTranslation().getX());
         properties.put("translationY", getTranslation().getY());
-        properties.put("globalRotation", getGlobalRotation());
         properties.put("globalRotationDegrees", getGlobalRotationDegrees());
-        properties.put("rotation", getRotation());
+        properties.put("localRotationDegrees", getLocalRotationDegrees());
         properties.put("rotationDegrees", getRotationDegrees());
         properties.put("globalScaleX", getGlobalScale().getX());
         properties.put("globalScaleY", getGlobalScale().getY());
+        Point localScale = getLocalScale();
+        if (null != localScale) {
+            properties.put("localScaleX", localScale.getX());
+            properties.put("localScaleY", localScale.getY());
+        }
         properties.put("scaleX", getScale().getX());
         properties.put("scaleY", getScale().getY());
         properties.put("isVisible", isVisible());
         return properties;
+    }
+
+    public void setProperties(Properties properties) {
+        if (null != properties) {
+            Object object = properties.get("type");
+            if (object instanceof String && ((String) object).equalsIgnoreCase(getType())) {
+                object = properties.get("name");
+                if (object instanceof String) {
+                    setName((String) object);
+                }
+
+                object = properties.get("x");
+                if (object instanceof Number) {
+                    setLocation((double) object, getLocation().getY());
+                } else if (object instanceof String) {
+                    setLocation(Double.parseDouble((String) object), getLocation().getY());
+                }
+                object = properties.get("y");
+                if (object instanceof Number) {
+                    setLocation(getLocation().getX(), (double) object);
+                } else if (object instanceof String) {
+                    setLocation(getLocation().getX(), Double.parseDouble((String) object));
+                }
+
+                object = properties.get("globalTranslationX");
+                if (object instanceof Number) {
+                    setGlobalTranslation((double) object, getGlobalTranslation().getY());
+                } else if (object instanceof String) {
+                    setGlobalTranslation(Double.parseDouble((String) object), getGlobalTranslation().getY());
+                }
+                object = properties.get("globalTranslationY");
+                if (object instanceof Number) {
+                    setGlobalTranslation(getGlobalTranslation().getX(), (double) object);
+                } else if (object instanceof String) {
+                    setGlobalTranslation(getGlobalTranslation().getX(), Double.parseDouble((String) object));
+                }
+                object = properties.get("localTranslationX");
+                if (object instanceof Number) {
+                    setLocalTranslation((double) object, getLocalTranslation().getY());
+                } else if (object instanceof String) {
+                    setLocalTranslation(Double.parseDouble((String) object), getLocalTranslation().getY());
+                }
+                object = properties.get("localTranslationY");
+                if (object instanceof Number) {
+                    setLocalTranslation(getLocalTranslation().getX(), (double) object);
+                } else if (object instanceof String) {
+                    setLocalTranslation(getLocalTranslation().getX(), Double.parseDouble((String) object));
+                }
+                object = properties.get("translationX");
+                if (object instanceof Number) {
+                    setTranslation((double) object, getTranslation().getY());
+                } else if (object instanceof String) {
+                    setTranslation(Double.parseDouble((String) object), getTranslation().getY());
+                }
+                object = properties.get("translationY");
+                if (object instanceof Number) {
+                    setTranslation(getTranslation().getX(), (double) object);
+                } else if (object instanceof String) {
+                    setTranslation(getTranslation().getX(), Double.parseDouble((String) object));
+                }
+
+                object = properties.get("globalRotationDegrees");
+                if (object instanceof Number) {
+                    setGlobalRotationDegrees((double) object);
+                } else if (object instanceof String) {
+                    setGlobalRotationDegrees(Double.parseDouble((String) object));
+                }
+                object = properties.get("localRotationDegrees");
+                if (object instanceof Number) {
+                    setLocalRotationDegrees((double) object);
+                } else if (object instanceof String) {
+                    setLocalRotationDegrees(Double.parseDouble((String) object));
+                }
+                object = properties.get("rotationDegrees");
+                if (object instanceof Number) {
+                    setRotationDegrees((double) object);
+                } else if (object instanceof String) {
+                    setRotationDegrees(Double.parseDouble((String) object));
+                }
+
+                object = properties.get("globalScaleX");
+                if (object instanceof Number) {
+                    setGlobalScale((double) object, getGlobalScale().getY());
+                } else if (object instanceof String) {
+                    setGlobalScale(Double.parseDouble((String) object), getGlobalScale().getY());
+                }
+                object = properties.get("globalScaleY");
+                if (object instanceof Number) {
+                    setGlobalScale(getGlobalScale().getX(), (double) object);
+                } else if (object instanceof String) {
+                    setGlobalScale(getGlobalScale().getX(), Double.parseDouble((String) object));
+                }
+                object = properties.get("localScaleX");
+                if (object instanceof Number) {
+                    setLocalScale((double) object, getLocalScale().getY());
+                } else if (object instanceof String) {
+                    setLocalScale(Double.parseDouble((String) object), getLocalScale().getY());
+                }
+                object = properties.get("localScaleY");
+                if (object instanceof Number) {
+                    setLocalScale(getLocalScale().getX(), (double) object);
+                } else if (object instanceof String) {
+                    setLocalScale(getLocalScale().getX(), Double.parseDouble((String) object));
+                }
+                object = properties.get("scaleX");
+                if (object instanceof Number) {
+                    setScale((double) object, getScale().getY());
+                } else if (object instanceof String) {
+                    setScale(Double.parseDouble((String) object), getScale().getY());
+                }
+                object = properties.get("scaleY");
+                if (object instanceof Number) {
+                    setScale(getScale().getX(), (double) object);
+                } else if (object instanceof String) {
+                    setScale(getScale().getX(), Double.parseDouble((String) object));
+                }
+
+                object = properties.get("isVisible");
+                if (object instanceof Boolean) {
+                    setVisible((Boolean) object);
+                } else if (object instanceof String) {
+                    setVisible(Boolean.parseBoolean((String) object));
+                }
+            }
+        }
     }
 }
